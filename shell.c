a1d572e55401241d6dcb78dd418fbfafef4d629e
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-01-09 20:01:16 +0800

    Initiating the project simple shell.

--- shell.c
+++ shell.c
@@ -4,6 +4,7 @@
  * @argc: Number of parameters passed
  * @argv: Array containing the list of parameters
  * @env: Array containing the environment variables
+ * Description: Here is the main source file for entring the program
  * Return: Always returns 0
  */
 int main(int argc, char **argv, char **env)
