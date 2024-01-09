c31a3dfd8e5d5502487d6cfb19b0d0efbbd17720
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-01-09 19:56:58 +0800

    Initiating the project simple shell.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # simple_shell
-ALX simple shell group project repository.
+    This project is a continuation of the check in the
+evolution of the skills as we continue in the SE Foundation
+ALX. The SIMPLE SHELL project :).
---
+++ shell.c
@@ -0,0 +1,12 @@
+#include "shell.h"
+/**
+ * main - Entry point
+ * @argc: Number of parameters passed
+ * @argv: Array containing the list of parameters
+ * @env: Array containing the environment variables
+ * Return: Always returns 0
+ */
+int main(int argc, char **argv, char **env)
+{
+return (0);
+}
